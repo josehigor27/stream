750e7f8689dd49511e85e24969841128d722ac69
josehigor27 <josehigor27@gmail.com>
2023-09-15 14:25:31 -0300

    feat: Exercícios

--- src/main/java/Desafios/Desafio2.java
+++ src/main/java/Desafios/Desafio2.java
@@ -3,7 +3,10 @@ package Desafios;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
-
+/*
+* Desafio 2 - Imprima a soma dos números pares da lista:
+Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
+* */
 public class Desafio2 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
